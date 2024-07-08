3a3bd3428a8636197c2e15f5651b087cdc5e8f5c
Robert Hall <rhall477@gmail.com>
2024-07-08 20:45:22 +1200

    setup routing to a new homepage and fixed tailwindcss. The issue was the output.css

---
+++ client/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  console.log('test')
+  return (
+    <div className="bg-background h-96">
+      <div className="font-poppins text-center">
+        <h1>Thyme Track</h1>
+      </div>
+    </div>
+  )
+}
