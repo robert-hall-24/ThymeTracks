79d0cd6cbb225d5cef4ae85b827766a90360cfa5
Robert Hall <rhall477@gmail.com>
2024-07-08 22:02:05 +1200

    still wokring on styling

--- client/pages/Home/Home.tsx
+++ client/pages/Home/Home.tsx
@@ -1,9 +1,19 @@
 export default function Home() {
   console.log('test')
   return (
-    <div className="bg-background h-96">
-      <div className="font-poppins text-center">
+    <div className="bg-green-600 text-white">
+      <div className="bg-background font-poppins text-center text-font">
         <h1>Thyme Track</h1>
+        <div className="grid grid-cols-4 gap-4">
+          <div className="bg-tile p-4 text-gray-800">1</div>
+          <div className="bg-tile-200 p-4 text-gray-800">2</div>
+          <div className="bg-gray-200 p-4 text-gray-800">3</div>
+          <div className="bg-gray-200 p-4 text-gray-800">4</div>
+          <div className="bg-gray-200 p-4 text-gray-800">5</div>
+          <div className="bg-gray-200 p-4 text-gray-800">6</div>
+          <div className="bg-gray-200 p-4 text-gray-800">7</div>
+          <div className="bg-gray-200 p-4 text-gray-800">8</div>
+        </div>
       </div>
     </div>
   )
